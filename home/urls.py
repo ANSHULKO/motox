39508f64aa52097ac554c2c67aec4df99de88fcb
ANSHUMAN DWIVEDI <DUBEYANSHU555@GMAIL.COM>
2022-04-06 17:43:33 +0530

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from .views import *
+urlpatterns = [
+    path('', index_view, name='index'),
+    path('blog/',TemplateView.as_view(template_name='home/blog.html')),
+    path('about/',TemplateView.as_view(template_name='home/about.html')),
+    path('contact/',TemplateView.as_view(template_name='home/contact.html')),
+    path('services/',TemplateView.as_view(template_name='home/services.html')),
+    path('testimonial/',TemplateView.as_view(template_name='home/testimonial.html')),
+    path('login/',TemplateView.as_view(template_name='accounts/login.html')),
+    path('register/',TemplateView.as_view(template_name='accounts/register.html')),
+
+]
